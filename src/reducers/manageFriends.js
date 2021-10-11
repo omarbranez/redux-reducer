53aba6cfef3226a7e07d0d07db798eec5537bb6b
Omar <omar.branez@gmail.com>
2021-10-11 16:16:42 -0400

    commit tree 687a7e4bb815ab08ae2be7ac6e65d12dd4bac142

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,18 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: [] }, action){
+    switch(action.type) {
+        case "ADD_FRIEND":
+            return Object.assign({}, state, {
+                ...state, // we might add state keys later
+                friends: [ ...state.friends, //the previous ones
+                    action.friend ]// friend is keyvalue pairs
+            })
+        case "REMOVE_FRIEND":
+            let notFriend = state.friends.findIndex(friend => friend.id === action.id)
+            return ( {...state, friends: [
+                ...state.friends.slice(0, notFriend),
+                ...state.friends.slice(notFriend + 1)
+            ]})
+        default: 
+            return state
+    }
 }
